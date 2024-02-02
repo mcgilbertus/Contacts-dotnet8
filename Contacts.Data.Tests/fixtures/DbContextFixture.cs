01319220d4009c1746faf3b7c6cf8da7c150b6e7
Ernesto Cullen <ernestocullen@gmail.com>
2024-02-02 18:25:13 -0500

    extracted DbContext creation to a fixture. Could not make DeleteAll work

---
+++ Contacts.Data.Tests/fixtures/DbContextFixture.cs
@@ -0,0 +1,26 @@
+﻿using Contacts.data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Contacts.Data.Tests.fixtures;
+
+public class DbContextFixture: IDisposable
+{
+    private readonly IConfiguration _config;
+    public DbContext Context { get; private set; }
+
+    public DbContextFixture()
+    {
+        var configFixture = new LocalConfigFixture();
+        _config = configFixture.Configuration;
+        var dbOptions = new DbContextOptionsBuilder<ContactsDbContext>();
+        var conString = _config.GetConnectionString("ContactsTestDb");
+        dbOptions.UseSqlServer(conString);
+        Context = new ContactsDbContext(dbOptions.Options);
+    }
+
+    public void Dispose()
+    {
+        Context?.Dispose();
+    }
+}
